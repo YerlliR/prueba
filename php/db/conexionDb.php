e49023a450a41cbd9d65b5e8d267fdcfaa602ea6
Sergio Ricart Alabau <sergiricartt@gmail.com>
2025-06-05 14:46:40 +0200

    Update conexionDb.php

--- php/db/conexionDb.php
+++ php/db/conexionDb.php
@@ -1,19 +1,12 @@
 <?php
     class conexionDb {
-        private $host = 'localhost';
-        private $db = 'remoteorder';
-        private $user = 'root';
-        private $password = '';
-
-        /*
 
         private $host = '127.0.0.1:3306';
         private $db = 'u248962232_remoteorder';
         private $user = 'u248962232_remoteorder';
         private $password = 'u248962232_remoteordeR';
 
-        */
-
+        
         private $conn;
 
         public function __construct() {
